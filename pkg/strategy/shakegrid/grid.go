61b0abe4776a7d20b50a5539defed5ac2db74213
c9s <yoan@maicoin.com>
2021-10-29 14:00:55 +0800

    test grid extend functions

--- pkg/strategy/shakegrid/grid.go
+++ pkg/strategy/shakegrid/grid.go
@@ -1,16 +1,18 @@
 package shakegrid
 
-import "github.com/c9s/bbgo/pkg/fixedpoint"
+import (
+	"github.com/c9s/bbgo/pkg/fixedpoint"
+)
 
 type Grid struct {
 	UpperPrice fixedpoint.Value `json:"upperPrice"`
 	LowerPrice fixedpoint.Value `json:"lowerPrice"`
 
-	// Size is the spread of each grid
-	Size         fixedpoint.Value `json:"size"`
+	// Spread is the spread of each grid
+	Spread fixedpoint.Value `json:"spread"`
 
-	// Density is the number of total grids
-	Density      fixedpoint.Value `json:"density"`
+	// Size is the number of total grids
+	Size fixedpoint.Value `json:"size"`
 
 	// Pins are the pinned grid prices, from low to high
 	Pins []fixedpoint.Value `json:"pins"`
@@ -21,23 +23,47 @@ func NewGrid(lower, upper, density fixedpoint.Value) *Grid {
 	var size = height.Div(density)
 	var pins []fixedpoint.Value
 
-	for p := lower ; p <= upper ; p += size {
+	for p := lower; p <= upper; p += size {
 		pins = append(pins, p)
 	}
 
 	return &Grid{
 		UpperPrice: upper,
 		LowerPrice: lower,
-		Density: density,
-		Size: size,
-		Pins: pins,
+		Size:       density,
+		Spread:     size,
+		Pins:       pins,
 	}
 }
 
-func (g *Grid) ExtendUpperPrice(price fixedpoint.Value) {
+func (g *Grid) ExtendUpperPrice(upper fixedpoint.Value) (newPins []fixedpoint.Value) {
+	g.UpperPrice = upper
 
+	// since the grid is extended, the size should be updated as well
+	g.Size = (g.UpperPrice - g.LowerPrice).Div(g.Spread).Ceil()
+
+	lastPin := g.Pins[ len(g.Pins) - 1 ]
+	for p := lastPin + g.Spread; p <= g.UpperPrice; p += g.Spread {
+		newPins = append(newPins, p)
+	}
+
+	g.Pins = append(g.Pins, newPins...)
+	return newPins
 }
 
-func (g *Grid) ExtendLowerPrice(price fixedpoint.Value) {
+func (g *Grid) ExtendLowerPrice(lower fixedpoint.Value) (newPins []fixedpoint.Value) {
+	g.LowerPrice = lower
 
+	// since the grid is extended, the size should be updated as well
+	g.Size = (g.UpperPrice - g.LowerPrice).Div(g.Spread).Ceil()
+
+	firstPin := g.Pins[0]
+	numToAdd := (firstPin - g.LowerPrice).Div(g.Spread).Ceil()
+
+	for p := firstPin - g.Spread.Mul(numToAdd); p < firstPin; p += g.Spread {
+		newPins = append(newPins, p)
+	}
+
+	g.Pins = append(newPins, g.Pins...)
+	return newPins
 }
